2fd5d5401975bcd2825f54dd217f72fb61ed423c
李金虎 <beagem@nhn.com>
2013-12-20 06:55:02 +0000

    Import of CUBRID/DBD-cubrid-8.4.4.0001 from CPAN.
    gitpan-cpan-distribution: DBD-Cubrid
    gitpan-cpan-version:      8.4.4.0001
    gitpan-cpan-path:         CUBRID/DBD-cubrid-8.4.4.0001.tar.gz
    gitpan-cpan-author:       CUBRID
    gitpan-cpan-maturity:     released

--- cubrid.h
+++ cubrid.h
@@ -52,16 +52,11 @@ DBISTATE_DECLARE;
 
 /* CUBRID types */
 
-#define CUBRID_ER_START                     -30000
-#define CUBRID_ER_CANNOT_GET_COLUMN_INFO    -30001
-#define CUBRID_ER_CANNOT_FETCH_DATA         -30002
-#define CUBRID_ER_WRITE_FILE                -30003
-#define CUBRID_ER_READ_FILE                 -30004
-#define CUBRID_ER_NOT_LOB_TYPE              -30005
-#define CUBRID_ER_INVALID_PARAM             -30006
-#define CUBRID_ER_ROW_INDEX_EXCEEDED        -30007
-#define CUBRID_ER_EXPORT_NULL_LOB_INVALID   -30008 
-#define CUBRID_ER_END                       -31000
+#define CUBRID_ER_CANNOT_GET_COLUMN_INFO    -2001
+#define CUBRID_ER_CANNOT_FETCH_DATA         -2002
+#define CUBRID_ER_WRITE_FILE                -2003
+#define CUBRID_ER_READ_FILE                 -2004
+#define CUBRID_ER_NOT_LOB_TYPE              -2005
 
 /* end of cubrid.h */
 
